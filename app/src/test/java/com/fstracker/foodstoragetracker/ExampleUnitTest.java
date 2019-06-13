272c55840734291eb2b933f6d1b5df5e254a08de
727021 <1834544+727021@users.noreply.github.com>
2019-06-12 18:04:07 -0600

    Add unit tests

--- app/src/test/java/com/fstracker/foodstoragetracker/ExampleUnitTest.java
+++ app/src/test/java/com/fstracker/foodstoragetracker/ExampleUnitTest.java
@@ -2,6 +2,10 @@ package com.fstracker.foodstoragetracker;
 
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +15,55 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void isExpiredShouldWork() {
+        try {
+            FoodItem f = new FoodItem();
+            SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+            Date d = format.parse("06/11/2019");
+            f.setExpirationDate(d);
+            assertTrue(f.isExpired());
+
+            d = format.parse("06/13/2019");
+            f.setExpirationDate(d);
+            assertFalse(f.isExpired());
+
+            f.setExpirationDate(format.parse("06/12/2019"));
+            assertTrue(f.isExpired());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void itemShouldBeSaved() {
+        assertNotNull(StorageManager.getLocalStorage());
+
+        FoodItem f = new FoodItem();
+        f.setName("Name");
+        f.setCategory(Category.CAT_1);
+        f.setUnits(Unit.UNIT_1);
+        f.setExpirationDate(new Date());
+        f.setQuantity(10);
+
+        int i = StorageManager.getLocalStorage().saveItem(f);
+        assertEquals(1, i);
+    }
+
+    @Test
+    public void itemShouldBeDeleted() {
+        assertNotNull(StorageManager.getLocalStorage());
+
+        FoodItem f = new FoodItem();
+        f.setName("Name");
+        f.setCategory(Category.CAT_1);
+        f.setUnits(Unit.UNIT_1);
+        f.setExpirationDate(new Date());
+        f.setQuantity(10);
+
+        int i = StorageManager.getLocalStorage().saveItem(f);
+        assertEquals(1, i);
+
+        i = StorageManager.getLocalStorage().deleteItem(f);
+        assertEquals(1, i);
     }
 }
