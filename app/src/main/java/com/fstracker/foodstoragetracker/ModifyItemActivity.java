7818dc3b863257e38680a1db5f0b4578f86c5904
Espinoza-Carlos <esp16013@byui.edu>
2019-06-21 23:27:02 -0700

    added Modify item categories layout created and added the add item

---
+++ app/src/main/java/com/fstracker/foodstoragetracker/ModifyItemActivity.java
@@ -0,0 +1,72 @@
+package com.fstracker.foodstoragetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class ModifyItemActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+    private TextView mDisplayDate2;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_item);
+                /*
+         * This will create the text View for the Expiration Date
+         */
+        mDisplayDate2 = (TextView) findViewById(R.id.tvDate2);
+        mDisplayDate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog2 = new DatePickerDialog(
+                        ModifyItemActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog2.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog2.show();
+            }
+        });
+        // Fill the category spinner
+        Spinner spnSearchCategory3 = findViewById(R.id.spnSearchCategory3); 
+        ArrayAdapter<Category> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Category.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSearchCategory3.setAdapter(adapter);
+
+        // Fill the FoodUnit spinner
+        Spinner spnSearchFoodUnit2 = findViewById(R.id.spnSearchFoodUnit2);
+        ArrayAdapter<Food_Unit> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Food_Unit.values());
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSearchFoodUnit2.setAdapter(adapter2);
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month = month +1 ;
+                Log.d(TAG, "onDateSet: mm/dd/yyy " + month + "/" + dayOfMonth + "/" + year);
+                //the variable date has the date.
+                String date = month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate2.setText(date);
+
+            }
+        };
+    }
+}
