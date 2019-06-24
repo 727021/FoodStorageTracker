a8d6e030f2097472046b32b7b384207a907f0bb2
727021 <1834544+727021@users.noreply.github.com>
2019-06-24 15:20:29 -0600

    Add LocalStorage class with stub functions

--- app/src/main/java/com/fstracker/foodstoragetracker/StorageManager.java
+++ app/src/main/java/com/fstracker/foodstoragetracker/StorageManager.java
@@ -3,15 +3,15 @@ package com.fstracker.foodstoragetracker;
 public class StorageManager {
     private StorageManager() {}
 
-    private static IStorageManager localStorage;
-    private static IStorageManager cloudStorage;
-/*
+    private static LocalStorage localStorage;
+//    private static CloudStorage cloudStorage;
+
     public static LocalStorage getLocalStorage() {
         if (localStorage == null)
             localStorage = new LocalStorage();
         return localStorage;
     }
-
+/*
     public static CloudStorage getCloudStorage() {
         if (cloudStorage == null)
             cloudStorage = new CloudStorage();
