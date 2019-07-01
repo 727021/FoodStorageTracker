a02731143b90a436c3b2281110287d226d762ad0
727021 <1834544+727021@users.noreply.github.com>
2019-07-01 08:51:42 -0600

    Update Unit and Category

--- app/src/main/java/com/fstracker/foodstoragetracker/Category.java
+++ app/src/main/java/com/fstracker/foodstoragetracker/Category.java
@@ -4,20 +4,16 @@ package com.fstracker.foodstoragetracker;
  * An enumeration of categories that FoodItems can belong to.
  */
 public enum Category {
-    // Temporary categories (these will change)
     ALL("All"),
-    CAT_2("Baking Mixes"),
-    CAT_3("Can"),
-    CAT_4("Cereals"),
-    CAT_5("Dairy"),
-    CAT_6("Drink Mixes"),
-    CAT_7("Fruits"),
-    CAT_8("Grains & Flours"),
-    CAT_9("Meats & Proteins"),
-    CAT_10("Produce"),
-    CAT_11("Vegetables");
-
-
+    CAN("Canned Foods"),
+    CEREAL("Cereals"),
+    DAIRY("Dairy"),
+    DRINK("Drinks"),
+    FRUIT("Fruits"),
+    GRAIN("Grains & Flours"),
+    MEAT("Meats & Proteins"),
+    PRODUCE("Produce"),
+    MISC("Miscellaneous");
 
     private String name;
 
--- app/src/main/java/com/fstracker/foodstoragetracker/Unit.java
+++ app/src/main/java/com/fstracker/foodstoragetracker/Unit.java
@@ -5,17 +5,17 @@ package com.fstracker.foodstoragetracker;
  */
 public enum Unit {
     COUNT("Count", "ct"),
-    CUP("Cup", "c"),
     FLUID_OUNCE("Fluid Ounce", "fl oz"),
+    CUP("Cup", "c"),
+    QUART("Quart", "qt"),
+    PINT("Pint", "pt"),
     GALLON("Gallon", "gal"),
+    MILLILITER("Milliliter", "mL"),
+    LITER("Liter", "L"),
     GRAM("Gram", "g"),
     KILOGRAM("Kilogram", "Kg"),
-    LITER("Liter", "L"),
-    MILLIGRAM("Milligram", "mg"),
     OUNCE("Ounce", "oz"),
-    PINT("Pint", "pt"),
-    POUND("Pound", "lbs"),
-    QUART("Quart", "qt");
+    POUND("Pound", "lbs");
 
     private String name;
     private String symbol;
@@ -25,9 +25,7 @@ public enum Unit {
         this.symbol = symbol;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public String getSymbol() {
         return symbol;
@@ -35,7 +33,6 @@ public enum Unit {
 
     @Override
     public String toString() {
-        // This can be changed once we know how we want units displayed
         return String.format("%s (%s)", name, symbol);
     }
 }
