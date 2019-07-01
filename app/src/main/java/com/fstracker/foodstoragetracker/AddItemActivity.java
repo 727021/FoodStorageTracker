e5ba7248aa8b51203fbbceedc6fa17962cb34494
727021 <1834544+727021@users.noreply.github.com>
2019-07-01 08:39:50 -0600

    Rename some variables and update code to follow style guide

--- app/src/main/java/com/fstracker/foodstoragetracker/AddItemActivity.java
+++ app/src/main/java/com/fstracker/foodstoragetracker/AddItemActivity.java
@@ -22,32 +22,32 @@ import com.google.gson.Gson;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
 
 public class AddItemActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
     private TextView mDisplayDate;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-    ///////
-    EditText NameEditText,DescriptionText,CountText;
-////
-    TextView textViewName;
 
+    EditText nameEditText;
+    EditText descriptionText;
+    EditText countText;
+    TextView textViewName;
     Button RegistrationButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        NameEditText=(EditText)findViewById(R.id.editText);
-        CountText=(EditText)findViewById(R.id.editText5);
-        DescriptionText=(EditText)findViewById(R.id.editText3);
-        textViewName=(TextView)findViewById(R.id.tvDate);
+        nameEditText = findViewById(R.id.editText);
+        countText = findViewById(R.id.editText5);
+        descriptionText = findViewById(R.id.editText3);
+        textViewName = findViewById(R.id.tvDate);
 
         /*
          * This will create the text View for the Expiration Date
          */
-        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+        mDisplayDate = findViewById(R.id.tvDate);
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,7 +82,7 @@ public class AddItemActivity extends AppCompatActivity {
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                month = month +1 ;
+                month = month + 1 ;
                 Log.d(TAG, "onDateSet: mm/dd/yyy " + month + "/" + dayOfMonth + "/" + year);
                 //the variable date has the date.
                 String date = month + "/" + dayOfMonth + "/" + year;
@@ -91,39 +91,37 @@ public class AddItemActivity extends AppCompatActivity {
             }
         };
 
-        Button button = (Button) findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                /////
-                final String Name= NameEditText.getText().toString();
-                final String word= CountText.getText().toString();
-                final String Count= DescriptionText.getText().toString();
+                final String name = nameEditText.getText().toString();
+                final String word = countText.getText().toString();
+                final String count = descriptionText.getText().toString();
                 final String textv = textViewName.getText().toString();
-                if(Name.length()==0) {
-                    NameEditText.requestFocus();
-                    NameEditText.setError("Field Cannot Be Empty");
+                if(name.length() == 0) {
+                    nameEditText.requestFocus();
+                    nameEditText.setError("Field Cannot Be Empty");
                 }
-                else if(textv.length() ==0) {
+                else if(textv.length() == 0) {
                     textViewName.requestFocus();
                     textViewName.setError("Field Cannot Be Empty");
                 }
-                else if(word.length() ==0) {
-                    CountText.requestFocus();
-                    CountText.setError("Field Cannot Be Empty");
+                else if(word.length() == 0) {
+                    countText.requestFocus();
+                    countText.setError("Field Cannot Be Empty");
                 }
-                else if(Count.length() ==0) {
-                    DescriptionText.requestFocus();
-                    DescriptionText.setError("Field Cannot Be Empty");
+                else if(count.length() == 0) {
+                    descriptionText.requestFocus();
+                    descriptionText.setError("Field Cannot Be Empty");
                 }
 
 
                 // DO WE WANT THE FOOD NAME WITHOUT NUMBERS?
                     /*else if(!Name.matches("[a-zA-Z]+"))
                 {
-                    NameEditText.requestFocus();
-                    NameEditText.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+                    nameEditText.requestFocus();
+                    nameEditText.setError("ENTER ONLY ALPHABETICAL CHARACTER");
                 }*/
                 else {
                     openAllItemActivity();
@@ -133,8 +131,6 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     public void openAllItemActivity() {
-        // TODO Validate input
-
         // Create a FoodItem
         FoodItem foodItem = new FoodItem();
         SimpleDateFormat sdf = new SimpleDateFormat(getResources().getStringArray(R.array.date_formats)[Settings.settings.dateFormat]);
@@ -150,7 +146,7 @@ public class AddItemActivity extends AppCompatActivity {
         Unit units = (Unit)((Spinner)findViewById(R.id.spnSearchFoodUnit)).getSelectedItem();
         foodItem.setUnits(units);
 
-        // TODO Save FoodItem to database *done*
+        // Save FoodItem to database
         Log.d(TAG, "food item contain: " + foodItem);
         StorageManager.getLocalStorage().saveItem(foodItem);
 
@@ -159,11 +155,10 @@ public class AddItemActivity extends AppCompatActivity {
         String json = new Gson().toJson(foodItem);
 
         // Start ViewItemActivity
-        Intent intent = new Intent (this, ViewItemActivity.class);
+        Intent intent = new Intent(this, ViewItemActivity.class);
         intent.putExtra(FoodItem.EXTRA, json);
         startActivity(intent);
         Log.d(TAG, "Saved Items: " + json);
         Toast.makeText(getApplicationContext(), "Food item stored", Toast.LENGTH_LONG).show();
     }
-
 }
